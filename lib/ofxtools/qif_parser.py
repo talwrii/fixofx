007f9a5b348285544f075b7fb4f0fa7585ab2568
Tom Wright <tom@tatw.name>
2016-01-31 20:52:31 +0000

    my editor still doesn't like your whitespace

--- lib/ofxtools/qif_parser.py
+++ lib/ofxtools/qif_parser.py
@@ -1,11 +1,11 @@
 # Copyright 2005-2010 Wesabe, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,7 @@ class QifParser:
                                 'B' : "Balance",
                                 '/' : "BalanceDate",
                                 '$' : "Balance" }
-        
+
         noninvestment_items = { 'D' : "Date",
                                 'T' : "Amount",
                                 'U' : "Amount2",
@@ -45,7 +45,7 @@ class QifParser:
                                 'E' : "SplitMemo",
                                 '$' : "SplitAmount",
                                 '-' : "NegativeSplitAmount" }
-        
+
         investment_items    = { 'D' : "Date",
                                 'N' : "Action",
                                 'Y' : "Security",
@@ -58,7 +58,7 @@ class QifParser:
                                 'O' : "Commission",
                                 'L' : "TransferAccount",
                                 '$' : "TransferAmount" }
-        
+
         category_items      = { 'N' : "Name",
                                 'D' : "Description",
                                 'T' : "TaxRelated",
@@ -66,61 +66,61 @@ class QifParser:
                                 'E' : "ExpenseCategory",
                                 'B' : "BudgetAmount",
                                 'R' : "TaxSchedule" }
-        
+
         class_items         = { 'N' : "Name",
                                 'D' : "Description" }
-        
+
         options   = Group(CaselessLiteral('!Option:') + restOfLine).suppress()
-        
-        banktxns  = Group(CaselessLiteral('!Type:Bank').suppress() + 
+
+        banktxns  = Group(CaselessLiteral('!Type:Bank').suppress() +
                           ZeroOrMore(Or([self._items(noninvestment_items),
                                          options]))
                           ).setResultsName("BankTransactions")
-        
-        cashtxns  = Group(CaselessLiteral('!Type:Cash').suppress() + 
+
+        cashtxns  = Group(CaselessLiteral('!Type:Cash').suppress() +
                           ZeroOrMore(Or([self._items(noninvestment_items),
                                          options]))
                           ).setResultsName("CashTransactions")
-        
+
         ccardtxns = Group(Or([CaselessLiteral('!Type:CCard').suppress(),
-                              CaselessLiteral('!Type!CCard').suppress()]) + 
+                              CaselessLiteral('!Type!CCard').suppress()]) +
                           ZeroOrMore(Or([self._items(noninvestment_items),
                                          options]))
                           ).setResultsName("CreditCardTransactions")
-        
-        liabilitytxns = Group(CaselessLiteral('!Type:Oth L').suppress() + 
+
+        liabilitytxns = Group(CaselessLiteral('!Type:Oth L').suppress() +
                           ZeroOrMore(Or([self._items(noninvestment_items),
                                          options]))
                           ).setResultsName("CreditCardTransactions")
-        
-        invsttxns = Group(CaselessLiteral('!Type:Invst').suppress() + 
+
+        invsttxns = Group(CaselessLiteral('!Type:Invst').suppress() +
                           ZeroOrMore(self._items(investment_items))
                           ).setResultsName("InvestmentTransactions")
-        
+
         acctlist  = Group(CaselessLiteral('!Account').suppress() +
                           ZeroOrMore(Or([self._items(account_items, name="AccountInfo")]))
                           ).setResultsName("AccountList")
-        
+
         category  = Group(CaselessLiteral('!Type:Cat').suppress() +
                           ZeroOrMore(self._items(category_items))
                           ).setResultsName("CategoryList")
-        
+
         classlist = Group(CaselessLiteral('!Type:Class').suppress() +
                           ZeroOrMore(self._items(category_items))
                           ).setResultsName("ClassList")
-        
+
         self.parser = Group(ZeroOrMore(White()).suppress() +
                             ZeroOrMore(acctlist).suppress() +
                             OneOrMore(ccardtxns | cashtxns | banktxns | liabilitytxns | invsttxns) +
                             ZeroOrMore(White()).suppress()
                             ).setResultsName("QifStatement")
-        
+
         if (debug):
-            self.parser.setDebugActions(ofxtools._ofxtoolsStartDebugAction, 
-                                        ofxtools._ofxtoolsSuccessDebugAction, 
+            self.parser.setDebugActions(ofxtools._ofxtoolsStartDebugAction,
+                                        ofxtools._ofxtoolsSuccessDebugAction,
                                         ofxtools._ofxtoolsExceptionDebugAction)
-        
-    
+
+
     def _items(self, items, name="Transaction"):
         item_list = []
         for (code, name) in items.iteritems():
@@ -130,12 +130,11 @@ class QifParser:
                      oneOf('^EUR ^').setResultsName('Currency') +
                      LineEnd().suppress()
                      ).setResultsName(name)
-    
+
     def _item(self, code, name):
         return CaselessLiteral(code).suppress() + \
                restOfLine.setResultsName(name) + \
                LineEnd().suppress()
-    
+
     def parse(self, qif):
         return self.parser.parseString(qif)
-    
